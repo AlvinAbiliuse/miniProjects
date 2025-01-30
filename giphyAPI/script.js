a875c45a248a68c653ea5119c69981af7125c2a4
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-30 05:38:10 +0400

    refactored code to use async and await

--- giphyAPI/script.js
+++ giphyAPI/script.js
@@ -7,21 +7,24 @@ let key = "sr3YmfucDXY2tWhybfwINnvqbsDSqSRw";
 let errorGIF =
 	"https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWpmM3FkYnptZ20xbjQwOWs4d3B0cjFieGVjd3Z3bDBvYzdrc3JoMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UHAYP0FxJOmFBuOiC2/giphy.gif";
 
-function getImage(search) {
+async function getImage(search) {
 	const img = document.querySelector("img");
-	fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${search}`, {
-		mode: "cors",
-	})
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (response) {
-			img.src = response.data.images.original.url;
-		})
-		.catch(function (message) {
-			console.error(message);
-			img.src = errorGIF;
+	img.removeAttribute("src");
+	img.classList.toggle("loader");
+	try {
+		let image = await fetch(
+			`https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${search}`,
+			{ mode: "cors" }
+		);
+		let response = image.json();
+		response.then((msg) => {
+			img.classList.toggle("loader");
+			img.setAttribute("src", msg.data.images.original.url);
 		});
+	} catch (e) {
+		console.log(e);
+		img.src = errorGIF;
+	}
 }
 
 getImage(s);
